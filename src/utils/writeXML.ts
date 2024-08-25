f2db80a84aaddd6296d0d8b39074fc361bb98103
nlanners <nicholas.lanners@gmail.com>
2024-08-25 15:58:15 -0600

    fix: remove new line from xml creation

--- src/utils/writeXML.ts
+++ src/utils/writeXML.ts
@@ -2,10 +2,5 @@ import type { ISAFRow } from 'src/types/ISAFRow.js';
 import { writeValues } from './writeValues.js';
 
 export const writeXML = (row: ISAFRow) => {
-  return `
-    <?xml version="1.0" encoding="UTF-8"?>
-      <dublin_core>
-      ${writeValues(row)}
-      </dublin_core>
-  `;
+  return `<?xml version="1.0" encoding="UTF-8"?><dublin_core>${writeValues(row)}</dublin_core>`;
 };
